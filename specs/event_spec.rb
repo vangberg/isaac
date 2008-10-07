b424d98fe91fb5ba96d48c4118dcb71fdba9e97d
ichverstehe <ichverstehe@gmail.com>
2008-10-07 11:14:40 +0200

    fixing event_spec

--- specs/event_spec.rb
+++ specs/event_spec.rb
@@ -4,8 +4,7 @@ include Isaac
 describe Event do
   it 'executes block' do
     event = Event.new(nil, lambda { msg 'arnie', 'hello you' })
-    event.invoke
-    event.commands.should eql(["PRIVMSG arnie :hello you"])
+    event.invoke.should eql(["PRIVMSG arnie :hello you"])
   end
   it 'executes block, passes params' do
     event = Event.new(/^say (.*)/, lambda do
@@ -13,7 +12,7 @@ describe Event do
       msg nick, message
     end)
 
-    event.invoke(:nick => 'arnie', :channel => '#awesome', :message => 'say this suckah')
-    event.commands.should eql(["PRIVMSG #awesome :arnie said: this suckah", "PRIVMSG arnie :say this suckah"])
+    event.invoke(:nick => 'arnie', :channel => '#awesome', :message => 'say this suckah').
+    should eql(["PRIVMSG #awesome :arnie said: this suckah", "PRIVMSG arnie :say this suckah"])
   end
 end
