4d23fd3e8e5eaf96ff9b4a128fb0a5deb0fbc60e
Harry Vangberg <harry@vangberg.name>
2009-03-02 00:22:08 +0100

    clean up tests

--- test/helper.rb
+++ test/helper.rb
@@ -7,13 +7,17 @@ begin
   require 'ruby-debug'
 rescue LoadError; end
 
+module Test::Unit::Assertions
+  def assert_empty_buffer(io)
+    assert_raise(Errno::EAGAIN) { io.read_nonblock 1 }
+  end
+end
+
 class Isaac::Bot
   include Test::Unit::Assertions
 end
 
 class Test::Unit::TestCase
-  include RR::Adapters::TestUnit
-
   def mock_bot(&b)
     bot = Isaac::Bot.new(&b)
     bot.config.environment = :test
@@ -34,9 +38,5 @@ class Test::Unit::TestCase
     assert_equal "NICK isaac\n", @server.gets
     assert_equal "USER isaac 0 * :Isaac\n", @server.gets
     1.upto(4) {|i| @server.puts ":localhost 00#{i}"}
-  end
-
-  def assert_empty_buffer(io)
-    assert_raise(Errno::EAGAIN) { io.read_nonblock 1 }
   end
 end
