e6dfff2193975191b2abd86460e59a62bccafb9e
Omachonu Ogali <oogali@gmail.com>
2011-09-11 09:30:29 -0400

    bumped version from 0.2.1 to 0.3.0

--- lib/isaac/bot.rb
+++ lib/isaac/bot.rb
@@ -1,7 +1,7 @@
 require 'eventmachine'
 
 module Isaac
-  VERSION = '0.2.1'
+  VERSION = '0.3.0'
 
   Config = Struct.new(:server, :port, :ssl, :password, :nick, :realname, :version, :environment, :verbose, :encoding)
 
