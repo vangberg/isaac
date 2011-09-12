d97742b2893de82d62baf8dd640d07a4eddba0c1
Omachonu Ogali <oogali@gmail.com>
2011-09-11 21:01:58 -0400

    added bind parameter for virtual host/source ip (defaults to 0.0.0.0)

--- lib/isaac/bot.rb
+++ lib/isaac/bot.rb
@@ -3,7 +3,7 @@ require 'eventmachine'
 module Isaac
   VERSION = '0.3.0'
 
-  Config = Struct.new(:server, :port, :ssl, :password, :nick, :realname, :version, :environment, :verbose, :encoding)
+  Config = Struct.new(:server, :port, :ssl, :password, :nick, :realname, :version, :environment, :verbose, :encoding, :bind)
 
   class Bot
     attr_accessor :config, :irc, :nick, :channel, :message, :user, :host, :match,
@@ -11,7 +11,7 @@ module Isaac
 
     def initialize(&b)
       @events = {}
-      @config = Config.new("localhost", 6667, false, nil, "isaac", "Isaac", 'isaac', :production, false, "utf-8")
+      @config = Config.new("localhost", 6667, false, nil, "isaac", "Isaac", 'isaac', :production, false, "utf-8", "0.0.0.0")
 
       instance_eval(&b) if block_given?
     end
@@ -118,7 +118,7 @@ module Isaac
 
   class IRC < EventMachine::Connection
     def self.connect(bot, config)
-      EventMachine.connect(config.server, config.port, self, bot, config)
+      EventMachine.bind_connect(config.bind, 0, config.server, config.port, self, bot, config)
     end
 
     def initialize(bot, config)
